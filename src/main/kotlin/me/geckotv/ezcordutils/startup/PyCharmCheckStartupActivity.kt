ce66592cdb66bf1c470b10ee0441d7793f849ac4
SilberGecko6917 <minecraftsider352@gmail.com>
2026-01-29 17:33:45 +0100

    fix: update plugin dependencies and improve startup activity check

--- src/main/kotlin/me/geckotv/ezcordutils/startup/PyCharmCheckStartupActivity.kt
+++ src/main/kotlin/me/geckotv/ezcordutils/startup/PyCharmCheckStartupActivity.kt
@@ -2,14 +2,14 @@ package me.geckotv.ezcordutils.startup
 
 import com.intellij.notification.NotificationGroupManager
 import com.intellij.notification.NotificationType
+import com.intellij.openapi.application.ApplicationNamesInfo
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.ProjectActivity
-import com.intellij.util.PlatformUtils
 
 class PyCharmCheckStartupActivity : ProjectActivity {
     override suspend fun execute(project: Project) {
-        @Suppress("UnstableApiUsage")
-        if (!PlatformUtils.isPyCharm()) {
+        val productName = ApplicationNamesInfo.getInstance().productName
+        if (!productName.contains("PyCharm")) {
             NotificationGroupManager.getInstance()
                 .getNotificationGroup("EzCord Utils Notifications")
                 .createNotification(
